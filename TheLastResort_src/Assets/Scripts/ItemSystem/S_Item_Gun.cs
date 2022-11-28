0d47d29a0020cec2803fa00ba9329327c8c6984a
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2022-11-28 02:07:06 +0100

    GunRelatedCommit
    reload, more sfx, decal damage.

--- TheLastResort_src/Assets/Scripts/ItemSystem/S_Item_Gun.cs
+++ TheLastResort_src/Assets/Scripts/ItemSystem/S_Item_Gun.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine;
 using System;
+using TMPro;
 
 public class S_Item_Gun : MonoBehaviour
 {
@@ -11,16 +12,27 @@ public class S_Item_Gun : MonoBehaviour
     private void Start()
     {
         item = GetComponent<Item>();
+
+        textAmmo = transform.parent.parent.GetComponent<S_playerCamera>().textAmmo;
+        textTotAmmo = transform.parent.parent.GetComponent<S_playerCamera>().textTotAmmo;
     }
 
     public int damage = 15;
     public string type = "bolt";
 
+    public int totAmmo;
     public int ammo;
     public int mag;
 
     public float recoil = 1f;
     float nextFire = 0;
+
+    public float reloadSpeed = 1f;
+    public float nextReloadSpeed = 0;
+    public bool isReload = false;
+
+    public TMP_Text textAmmo;
+    public TMP_Text textTotAmmo;
 
     private void Update()
     {
@@ -31,30 +43,109 @@ public class S_Item_Gun : MonoBehaviour
                 case "bolt":
                     if (Input.GetMouseButtonDown(0) && Time.time >= nextFire)
                     {
-                        nextFire = Time.time + recoil;
-                        shoot();
-                        GetComponent<S_Recoil>().trigger();
+                        if (ammo != 0)
+                        {
+                            nextFire = Time.time + recoil;
+                            shoot();
+                            ammo -= 1;
+                            GetComponent<S_Recoil>().trigger();
+                        }
+                        else
+                        {
+                            AudioSource.PlayClipAtPoint(emptySFX, transform.position);
+                        }
+                        textAmmo.text = ammo.ToString();
+                        textTotAmmo.text = totAmmo.ToString();
+                    }
+                    break;
+                case "auto":
+                    if (Input.GetMouseButton(0) && Time.time >= nextFire)
+                    {
+                        if (ammo != 0)
+                        {
+                            nextFire = Time.time + recoil;
+                            shoot();
+                            ammo -= 1;
+                            GetComponent<S_Recoil>().trigger();
+                        }
+                        else
+                        {
+                            AudioSource.PlayClipAtPoint(emptySFX, transform.position);
+                        }
+                        textAmmo.text = ammo.ToString();
+                        textTotAmmo.text = totAmmo.ToString();
                     }
                     break;
                 default:
                     break;
             }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                if (totAmmo > 0)
+                {
+                    nextReloadSpeed = Time.time + reloadSpeed;
+                    AudioSource.PlayClipAtPoint(clipOutSFX, transform.position);
+                    if (totAmmo >= mag && ammo < mag)
+                    {
+                        int c = mag - ammo;
+                        ammo = mag;
+                        totAmmo -= c;
+                    }
+                    else if(totAmmo < mag)
+                    {
+                        ammo = totAmmo;
+                        totAmmo = 0;
+                    }
+                    isReload = true;
+                    textAmmo.text = ammo.ToString();
+                    textTotAmmo.text = totAmmo.ToString();
+                }
+            }
+
+            if (Time.time >= nextReloadSpeed && isReload)
+            {
+                AudioSource.PlayClipAtPoint(clipInSFX, transform.position);
+                isReload = false;
+            }
         }
     }
 
     public AudioClip shootSFX;
+    public AudioClip emptySFX;
+    public AudioClip clipOutSFX;
+    public AudioClip clipInSFX;
+    public GameObject shotDecal;
+
+    [SerializeField] private Vector3 sprayOffset;
+
+    void randSpray()
+    {
+        sprayOffset = new Vector3(UnityEngine.Random.Range(-0.5f, 0.5f), UnityEngine.Random.Range(-0.5f, 0.5f), UnityEngine.Random.Range(-0.5f, 0.5f));
+    }
 
     private void shoot()
     {
         AudioSource.PlayClipAtPoint(shootSFX, transform.position);
+        randSpray();
 
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
+        if (Physics.Raycast(transform.parent.parent.position + sprayOffset, transform.parent.parent.forward, out hit, 100))
         {
             if (hit.transform.tag == "Damagable")
             {
                 hit.transform.GetComponent<S_Health>().damage(damage);
             }
+            GameObject go = Instantiate(shotDecal);
+            go.transform.position = hit.point;
+            go.transform.LookAt(transform.parent.parent.position);
+            StartCoroutine(killDecal(go));
         }
+    }
+
+    private IEnumerator killDecal(GameObject go)
+    {
+        yield return new WaitForSeconds(8);
+        Destroy(go);
     }
 }
